504c28429f3af4d3ecaedfb4d3d9989f4402ae1e
kayua <kayuaolequesp@gmail.com>
2024-10-24 01:31:01 -0300

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# OctoNerd AI - Learning generative AI 
-
-![Cute Cat](Presentation/splash_screen.png)
--- Scripts/NavBar.js
+++
@@ -1,3 +0,0 @@
-fetch('navbar.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('nav_bar').innerHTML = data);
